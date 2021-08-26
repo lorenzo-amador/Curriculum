58e34d90c87a97512b8e7fdb5d24893dfec64043
lorenzo-amador <83660292+lorenzo-amador@users.noreply.github.com>
2021-08-26 15:14:36 -0600

    Add files via upload

---
+++ js/sweetAlert.js
@@ -0,0 +1,14 @@
+Swal.fire({
+    title: 'Bienvenido',
+    text: 'Tendrá a su disposición mas datos sobre mi',
+    icon: 'info',
+    confirmButtonText: 'Entiendo',
+    width: `50%`,
+    background: `#faf2f2;`,
+    backdrop: `true`,
+    timer:`5000`,
+    iconColor:`0000`,
+    heightAuto:`auto`
+    // imageUrl:`img/foto-seccion.png`,
+    // imageWidth:`100px`
+  })
