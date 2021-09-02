3a89b76ae01e38b1006bb8a56719befc7b0d6c75
lorenzo-amador <83660292+lorenzo-amador@users.noreply.github.com>
2021-09-02 14:53:09 -0600

    Add files via upload

--- js/funciones.js
+++ js/funciones.js
@@ -37,3 +37,21 @@ $(document).ready(function(){
 		$('nav').toggleClass('active')
 	})
 })
+
+// SWIPER 
+ var swiper = new Swiper('.swiper', {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
