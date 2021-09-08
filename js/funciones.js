5c9a78ddf96546583e221de969c3ef44bcff63f4
lorenzo-amador <83660292+lorenzo-amador@users.noreply.github.com>
2021-09-07 21:36:32 -0600

    Update funciones.js

--- js/funciones.js
+++ js/funciones.js
@@ -15,14 +15,14 @@ $(document).ready(function(){
 	$('.ir-arriba').click(function(){
 		$('body, html').animate({
 			scrollTop: '0px'
-		}, 8000);
+		}, 3000);
 	});
 
 	$(window).scroll(function(){
 		if( $(this).scrollTop() > 0 ){
-			$('.ir-arriba').slideDown(8000);
+			$('.ir-arriba').slideDown(3000);
 		} else {
-			$('.ir-arriba').slideUp(5000);
+			$('.ir-arriba').slideUp(3000);
 		}
 	});
 });
