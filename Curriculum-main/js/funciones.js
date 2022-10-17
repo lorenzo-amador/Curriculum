de49c46a436914ed6d5b955554dbb3280e696891
lorenzo-amador <83660292+lorenzo-amador@users.noreply.github.com>
2022-10-16 20:18:19 -0600

    Add files via upload

---
+++ Curriculum-main/js/funciones.js
@@ -0,0 +1,46 @@
+Swal.fire({
+	title: `¡Hola!`,
+	text:`Bienvenido, a continuación tendrá a su disposición mas datos sobre mi`,
+	timer:2000,
+	showConfirmButton:false,
+	imageUrl:`img/alerta.webp`,
+	imageWidth:`20%`,
+	scrollbarPadding:false,
+});
+
+
+// BOTON ARRIBA
+$(document).ready(function(){
+
+	$('.ir-arriba').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 1000);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 400 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+});
+
+// SWIPER 
+ var swiper = new Swiper('.swiper', {
+      spaceBetween: 0,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
